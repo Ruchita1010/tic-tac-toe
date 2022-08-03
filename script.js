fb7bb58617c49597eb6694b5db3b8dd99fa95dce
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-08-03 22:47:41 +0530

    Add functions to display various screens
    Add functionality to allow players to set their name.
    Change some element's class name to be more appropriate. Fix small bugs.

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const displayController = (() => {
-    const modesContainer = document.querySelector(".modes-container");
-    const board = document.querySelector(".board");
+    // Getting all the required HTML elements
+    const modesScreen = document.querySelector(".modes-screen");
+    const gameBoardScreen = document.querySelector(".game-board-screen");
     const modes = document.querySelectorAll(".mode");
+    const nameInputScreen = document.querySelector(".name-input-screen");
+    const twoPlayerModeInput = document.querySelector(".two-player-mode-input");
+    const botModeInput = document.querySelector(".bot-mode-input");
+    const startBtn = document.querySelector("#start-btn");
 
-    const toggleScreen = () => {
-        modesContainer.classList.toggle("hide");
-        board.classList.toggle("show");
+    const displayNameInputScreen = (e) => {
+        modesScreen.classList.toggle("hide");
+        nameInputScreen.classList.toggle("show");
+        const mode = e.target.id;
+        switch (mode) {
+            case "two-player":
+                twoPlayerModeInput.classList.toggle("show");
+                break;
+            case "bot":
+                botModeInput.classList.toggle("show");
+                break;
+        }
     }
 
+    const displayGameBoardScreen = () => {
+        nameInputScreen.classList.toggle("show");
+        gameBoardScreen.classList.toggle("show");
+    }
+
+    // Event Listeners
     modes.forEach(mode => {
-        mode.addEventListener("click", toggleScreen);
+        mode.addEventListener("click", displayNameInputScreen);
     });
+    startBtn.addEventListener("click", displayGameBoardScreen);
 })();
